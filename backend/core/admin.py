34365dab4f7228eabdeec5aab77d5313f00349ee
Vansh Verma <99955312+vary2002@users.noreply.github.com>
2025-09-29 15:08:21 +0530

    Delete backend directory

--- backend/core/admin.py
+++
@@ -1,4 +0,0 @@
-# core/admin.py
-from django.contrib import admin
-from .models import Sector, Company, Report, Order, OrderItem, Purchase
-admin.site.register([Sector, Company, Report, Order, OrderItem, Purchase])
